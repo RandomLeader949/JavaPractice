35f8c0ec55515e39e724081e7ef4b0455426e6ab
RandomLeader949 <121531508+RandomLeader949@users.noreply.github.com>
2024-05-06 12:39:45 +0000

    Adds code to print

--- print.java
+++ print.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
 public class print
 {
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your age? ");
-        int age = scanner.nextInt();
-        System.out.println(String.valueOf(age));
+        System.out.println("hello world");
     }
 }
