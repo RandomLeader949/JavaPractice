959d3b656ad082dd7761b72c317acfc5180fad1d
RandomLeader949 <121531508+RandomLeader949@users.noreply.github.com>
2024-05-16 13:18:46 +0000

    Adds correct quickSum code

--- quickSum.java
+++ quickSum.java
@@ -1,17 +1,16 @@
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
-public class quickSum
-{
-    public static void main(String[] args)
-    {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number: ");
-        int n = scanner.nextInt();
-        int i = 1;
-        int answer = 0;
-        while(i <= n){
-            answer += i;
-            i++;
+public class quickSum{
+    public static void main(String[] args) throws IOException{
+        FileReader fr = new FileReader("quickSum.txt");
+        Scanner in = new Scanner(fr);
+        int num;
+        num = in.nextInt();
+        int end = 0;
+        for(int i = 0; i <= num; i++){
+            end += i;
         }
-        System.out.println(String.valueOf(answer));
+        System.out.println(String.valueOf(end));
     }
 }
