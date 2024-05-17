eec45abc5f0b906a45cc9e661fe1e0eb72314918
RandomLeader949 <121531508+RandomLeader949@users.noreply.github.com>
2024-05-17 14:00:43 +0000

    completed project euler #1 code

--- projectEuler1.java
+++ projectEuler1.java
@@ -3,6 +3,16 @@
 import java.io.IOException;
 public class projectEuler1 {
     public static void main(String[] args) throws IOException{
-        
+        int end = 1000;
+        int i = 0;
+        for(int q = 0; q < end; q++){
+            if(q%3 == 0){
+                i += q;
+            }
+            else if(q%5 == 0){
+                i += q;
+            }
+        }
+        System.out.println(String.valueOf(i));
     }
 }
