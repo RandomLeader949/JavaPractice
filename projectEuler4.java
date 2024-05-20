546e5dc1bffb719b3d6d4a41d8d9434b7c7deef7
RandomLeader949 <121531508+RandomLeader949@users.noreply.github.com>
2024-05-20 11:47:55 +0000

    project euler 6

--- projectEuler4.java
+++ projectEuler4.java
@@ -2,21 +2,36 @@ import java.io.IOException;
 
 public class projectEuler4 {
     public static void main(String[] args) throws IOException{
-        int k = 909;
-        int g = 0;
-        int l = k;
-        int i = 0;
-        while(l > 0){
-            g *= 10;
-            g += l%10;
-            l -= l%10;
-            l /= 10;
+        int a = 999;
+        int b = 999;
+        int forward = a * b;
+        int reduce = forward;
+        int backward = 0;
+        while(reduce > 0){
+            backward *= 10;
+            backward += reduce % 10;
+            reduce -= reduce % 10;
+            reduce /= 10;
         }
-        if(k == g){
-            System.out.println("ok");
+        System.out.println(String.valueOf(forward));
+        System.out.println(String.valueOf(backward));
+        while(backward != forward){
+ 
+                b--;
+                System.out.println(String.valueOf(forward) + ":" + backward);
+                forward = a * b;
+                reduce = forward;
+                backward = 0;
+                while(reduce > 0){
+                    backward *= 10;
+                    backward += reduce % 10;
+                    reduce -= reduce % 10;
+                    reduce /= 10;
+                }
+
         }
-        else{
-            System.out.println("no");
-        }
+        System.out.println(String.valueOf(forward));
+        System.out.println(String.valueOf(a));
+        System.out.println(String.valueOf(b));
     }        
 }
